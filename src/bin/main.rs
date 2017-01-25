483538c27bf74247b8454c1f6c6befb252fa9b96
Guillaume DIDIER <guillaume.didier@polytechnique.edu>
2017-01-25 11:37:23 +0100

    A few tests in main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,13 +1,26 @@
 extern crate C_teel;
+extern crate nom;
 
 use C_teel::parse;
 use C_teel::parse::ast;
+use nom::IResult;
+use std::boxed;
 
 fn main() {
-    let e : ast::UnaryOp = ast::UnaryOp::Not;
-    match e {
-        ast::UnaryOp::Not => println!("Not"),
-        ast::UnaryOp::Minus => println!("Minus"),
+    assert!(match parse::lexer::integer("0"){ IResult::Done(rem, ast::Expression::Int(i)) => i == 0 && rem == &""[..], _ => false});
+    assert!(match parse::lexer::integer("1"){ IResult::Done(rem, ast::Expression::Int(i)) => i == 1 && rem == &""[..], _ => false});
+    assert!(match parse::lexer::integer("10"){ IResult::Done(rem, ast::Expression::Int(i)) => i == 10 && rem == &""[..], _ => false});
+    assert!(match parse::lexer::integer("0x10"){ IResult::Done(rem, ast::Expression::Int(i)) => i == 16 && rem == &""[..], _ => false});
+    assert!(match parse::lexer::integer("011"){ IResult::Done(rem, ast::Expression::Int(i)) => i == 9 && rem == &""[..], _ => false});
+    //let res = match parse::lexer::identifier(" a1 "){
+    //    IResult::Done(rem, i) => i == "a1" && rem == &""[..],
+    //    _ => false
+    //};
+    //assert!(res);
+    match parse::parser::statement("a = b ;") {
+        IResult::Done(_,_) => println!("OK"),
+        IResult::Incomplete(_) => println!("INC"),
+        _ => println!("BAD", ),
     }
     println!("Hello, world!");
 }
