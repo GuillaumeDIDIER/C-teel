06fb122fc7fbdf6c3dfd6a6c26d438e432193979
Guillaume DIDIER <guillaume.didier@polytechnique.edu>
2017-03-17 17:01:57 +0100

    Clean up register lifetime calculation

--- src/ertl/liveness.rs
+++ src/ertl/liveness.rs
@@ -4,7 +4,6 @@ use ertl::Instruction;
 use std::collections::{HashMap, HashSet};
 use std::iter::FromIterator;
 use std::collections::VecDeque;
-//use std::collections::hash_set::Union;
 
 
 #[derive(Debug)]
@@ -19,12 +18,6 @@ pub struct LivenessInfo {
 
 impl LivenessInfo {
     pub fn compute(instructions: &HashMap<Label, Instruction>) -> HashMap<Label, LivenessInfo> {
-        /*let mut predecessors = HashMap::new();
-        let mut successors = HashMap::new();
-        let mut defs = HashMap::new();
-        let mut
-        let mut ins = HashMap::new();
-        let mut outs = HashMap:new();*/
 
         let mut liveness = HashMap::new();
         for (label, instruction) in instructions {
@@ -40,13 +33,10 @@ impl LivenessInfo {
         let mut working_set = VecDeque::from_iter(keys);
         while let Some(l) = working_set.pop_front() {
             let old_ins = liveness[&l].ins.clone();
-            println!("Begin Union\n", );
-            let outs = liveness[&l].successors.iter().fold(HashSet::new(), |acc, l| {println!("{:?}", &liveness[l].ins);acc.union(&liveness[l].ins).cloned().collect()});
-            println!("\n Result: {:?}\n", outs);
+            let outs = liveness[&l].successors.iter().fold(HashSet::new(), |acc, l| {acc.union(&liveness[l].ins).cloned().collect()});
             liveness.get_mut(&l).unwrap().outs = outs;
             let tmp = &liveness[&l].outs - &liveness[&l].defs;
-            let ins : HashSet<Register> = liveness[&l].uses.union(&tmp).cloned().collect();//use(l) ∪ (out(l)\def (l))
-            println!("{:?} {} {:?}", ins, if ins == old_ins {"=="} else {"!="}, old_ins);
+            let ins : HashSet<Register> = liveness[&l].uses.union(&tmp).cloned().collect();
             if ins != old_ins {
                 working_set.extend(liveness[&l].predecessors.iter());
                 liveness.get_mut(&l).unwrap().ins = ins;
